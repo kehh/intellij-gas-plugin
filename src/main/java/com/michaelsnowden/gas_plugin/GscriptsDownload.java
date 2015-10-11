d086470cd8c935f1173527d1cf993dfe4e9ac2a1
Michael Snowden <michael.snowden@glassdoor.com>
2015-10-11 06:30:52 -0700

    Removed GscriptPrinter

--- src/main/java/com/michaelsnowden/gas_plugin/GscriptsDownload.java
+++ src/main/java/com/michaelsnowden/gas_plugin/GscriptsDownload.java
@@ -4,20 +4,20 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.impl.file.PsiDirectoryFactory;
 
+import javax.swing.*;
 import java.io.IOException;
 
 /**
@@ -26,86 +26,51 @@ import java.io.IOException;
 public class GscriptsDownload extends AnAction {
     public void actionPerformed(AnActionEvent actionEvent) {
         try {
-            String projectId = "1VYICZ3y5MiF_4oMXvfQ12Mcov33rTR1EF3RNbrR4DhEmdYbsJSRKYR06";
-            GASProject gasProject = GASProject.downloadGASProject(DriveQuickstart.getDriveService(),
-                    projectId);
-            new PresentChooser().importGscriptProject(gasProject, actionEvent);
+            Project project = actionEvent.getProject();
+            assert project != null;
+            GASProject gasProject = getGasProject(project);
+            importGasProject(DataKeys.PSI_FILE.getData(actionEvent.getDataContext()), project, gasProject);
             System.out.println("");
         } catch (IOException e1) {
             e1.printStackTrace();
         }
     }
 
-    interface GscriptImporter {
-        void importGscriptProject(GASProject gasProject, AnActionEvent actionEvent);
+    private GASProject getGasProject(Project project) throws IOException {
+        final String message = "What is your GAS project ID?";
+        final String title = "Input Your GAS Project ID";
+        final Icon questionIcon = Messages.getQuestionIcon();
+        final String projectId = Messages.showInputDialog(project, message, title, questionIcon);
+        return GASProject.downloadGASProject(DriveQuickstart.getDriveService(), projectId);
     }
 
-    static class GscriptImporterImpl1 implements GscriptImporter {
-        @Override
-        public void importGscriptProject(GASProject gasProject, AnActionEvent actionEvent) {
-            String gscript = gasProject.getGasFiles().get(0).getSource();
-            Project currentProject = DataKeys.PROJECT.getData(actionEvent.getDataContext());
-            VirtualFile currentFile = DataKeys.VIRTUAL_FILE.getData(actionEvent.getDataContext());
-            Editor editor = DataKeys.EDITOR.getData(actionEvent.getDataContext());
-            FileType js = FileTypeManager.getInstance().getFileTypeByExtension("js");
-            PsiFile fileFromText = PsiFileFactory.getInstance(currentProject).createFileFromText("hello.js", js,
-                    gscript);
-            PsiDirectoryFactory.getInstance(currentProject).createDirectory(currentFile).add(fileFromText);
+    private void importGasProject(PsiFile currentFile, Project project, GASProject gasProject) {
+        VirtualFile chooseFile = project.getBaseDir();
+        if (currentFile != null) {
+            chooseFile = currentFile.getVirtualFile();
         }
-    }
-
-    static class GscriptImporterImpl2 implements GscriptImporter {
-        @Override
-        public void importGscriptProject(GASProject gasProject, AnActionEvent actionEvent) {
-            Project project = DataKeys.PROJECT.getData(actionEvent.getDataContext());
-            PsiDirectory containingDirectory = DataKeys.PSI_FILE.getData(actionEvent.getDataContext())
-                    .getContainingDirectory();
-            FileType gs = FileTypeManager.getInstance().getFileTypeByExtension("gs");
-            for (GASFile gasFile : gasProject.getGasFiles()) {
-                String source = gasFile.getSource();
-                String name = gasFile.getName() + ".gs";
-                PsiFile file = PsiFileFactory.getInstance(project).createFileFromText(name, gs, source);
-                containingDirectory.add(file);
-            }
+        FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
+        PsiManager psiManager = PsiManager.getInstance(project);
+        chooseFile = FileChooser.chooseFile(descriptor, project, chooseFile);
+        if (chooseFile == null) {
+            return;
         }
-    }
+        final PsiDirectory directory = psiManager.findDirectory(chooseFile);
 
-    static class PresentChooser implements GscriptImporter {
-        @Override
-        public void importGscriptProject(GASProject gasProject, AnActionEvent actionEvent) {
-            Project project = actionEvent.getProject();
-            assert project != null;
+        FileType gs = FileTypeManager.getInstance().getFileTypeByExtension("gs");
+        for (GASFile gasFile : gasProject.getGasFiles()) {
+            String source = gasFile.getSource();
+            String name = gasFile.getName() + ".gs";
+            final PsiFile file = PsiFileFactory.getInstance(project).createFileFromText(name, gs, source);
 
-
-
-            VirtualFile chooseFile = project.getBaseDir();
-            PsiFile currentFile = DataKeys.PSI_FILE.getData(actionEvent.getDataContext());
-            if (currentFile != null) {
-                chooseFile = currentFile.getVirtualFile();
-            }
-            FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
-            PsiManager psiManager = PsiManager.getInstance(project);
-            chooseFile = FileChooser.chooseFile(descriptor, project, chooseFile);
-            if (chooseFile == null) {
-                return;
-            }
-            final PsiDirectory directory = psiManager.findDirectory(chooseFile);
-
-            FileType gs = FileTypeManager.getInstance().getFileTypeByExtension("gs");
-            for (GASFile gasFile : gasProject.getGasFiles()) {
-                String source = gasFile.getSource();
-                String name = gasFile.getName() + ".gs";
-                final PsiFile file = PsiFileFactory.getInstance(project).createFileFromText(name, gs, source);
-
-                ApplicationManager.getApplication().runWriteAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (directory != null) {
-                            directory.add(file);
-                        }
+            ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                @Override
+                public void run() {
+                    if (directory != null) {
+                        directory.add(file);
                     }
-                });
-            }
+                }
+            });
         }
     }
 }
